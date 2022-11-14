629237cda8e14114cd7f40afb81ebbcbda14156e
Ritesh134340 <riteshkumar134340@gmail.com>
2022-11-14 08:31:47 +0530

    removed user from index

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const express = require("express");
 const app = express();
 const cors=require("cors")
 const {connection}=require("./config/db")
-const {userController}=require("./Routes/User.route")
-const { jobModel } = require("./Models/Job.model");
-const { adminController } = require("./Routes/Admin.route");
-// const { userModel } = require("./Models/user.model");
+
+
 
 const PORT=process.env.PORT || 8080
 
@@ -17,57 +15,6 @@ app.get("/", (req, res) => {
   res.send("welcome to the jobkar app");
 });
 
-app.use("/user", userController);
-app.use("/admin", adminController)
-
-app.get("/job", async (req, res) => {
-  console.log(req.query)
-  const query = req.query
-  
-    const job = await jobModel.find(query)
-  
-  res.send(job)
-
-})
-
-app.get("/job/:id", async (req, res) => {
-  console.log(req.params.id)
-  const id = req.params.id
-  try{
-
-    const job = await jobModel.findOne({_id:id})
-  
-  res.send(job)
-  }
-  catch(err){
-    console.log(err)
-  }
- 
-})
-
-
-
-
-
-// app.delete("/resgisteredusers/:id" , async(req,res)=>{
-//   const id= req.params.id ;
-//   try{
-
-//     await userModel.deleteOne({_id:id})
-  
-   
-//    res.send({msg:"User deleted successfully"}) 
-//   }
-//   catch(err){
-//     console.log(err)
-//   }
-
-// })
-
-// app.get("/registeredusers",async(req,res)=>{
-// const registeredusers= await userModel.find()
-// res.send(registeredusers)
-// })
 
 
 
